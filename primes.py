871cdcd583580f423a1a70af6a3922e7e1b5aa86
Ka Chun Chong <ka.c.chong@kcl.ac.uk>
2022-09-29 13:25:06 +0100

    Add ValueError when number_of_primes is less than 0

--- primes.py
+++ primes.py
@@ -3,4 +3,11 @@
 
 def primes(number_of_primes):
     list = []
+    num = 2
+    if number_of_primes <= 0:
+        raise ValueError(f'number_of_primes is {number_of_primes}. This value must be greater than 0 ')
+    while len(list) < number_of_primes:
+        if all(num%i != 0 for i in range(2, num)):
+            list.append(num)
+        num+=1
     return list
